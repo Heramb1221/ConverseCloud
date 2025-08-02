698b5020fbe130ad5e32a1415148268fa07b74b5
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-08-02 22:00:16 +0530

    Basic Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv';
 
+import authRoutes from './routes/auth.js';
+
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get('/api/auth/signup', (req, res) => {
-    res.send('Sighup Route');
-});
-
-app.get('/api/auth/login', (req, res) => {
-    res.send('Login Route');
-});
-
-app.get('/api/auth/logout', (req, res) => {
-    res.send('Logout Route');
-});
+app.use("/api/auth", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
