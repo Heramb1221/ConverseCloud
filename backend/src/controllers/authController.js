ce801d83e5272185a6db0891ce4f9e9c57c6bc6c
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-08-03 12:53:38 +0530

    Signin Route

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,63 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
 export async function signup(req, res) {
-    res.send('Sighup Route');
+    const {email, password, fullName} = req.body;
+
+    try {
+        if(!email || !password || !fullName) {
+            return res.status(400).json({error: 'All fields are required'});
+        }
+
+        if(password.length < 6) {
+            return res.status(400).json({error: 'Password must be at least 6 characters long'});
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ error: "Invalid email format" });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Email already exists, please login' });
+        }
+
+        const index = Math.floor(Math.random() * 100) + 1;
+        const randomAvatar = `https://avatar.iran.liara.run/public/${index}.png`;
+
+        const newUser = await User.create({
+            email,
+            password,
+            fullName,
+            profilePic: randomAvatar
+        });
+
+        const token = jwt.sign({userId:newUser._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('jwt', token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV === 'production'
+        })
+
+        res.status(201).json({
+            success: true,
+            message: 'User created successfully',
+            user: {
+                _id: newUser._id,
+                email: newUser.email,
+                fullName: newUser.fullName,
+                profilePic: newUser.profilePic
+            }
+        });
+
+    } catch (error) {
+        console.error('Error during signup:', error);
+        res.status(500).json({error: 'Internal server error'});
+    }
 }
 
 export async function login(req, res) {
