3d8882b661ea58d2d64edb3f3c8f745d037fe6bd
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-08-03 18:05:24 +0530

    Chat routes

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -20,5 +20,11 @@ export const upsertStreamUser = async (userData) => {
 };
 
 export const generateStreamToken = (userId) => {
-    
+    try {
+        const userIdStr = userId.toString();
+        return streamClient.createToken(userIdStr);
+    } catch (error) {
+        console.error("Error generating Stream token:", error);
+        throw new Error("Failed to generate Stream token");
+    }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.js';
 import { connectDB } from './lib/db.js';
 import userRoutes from './routes/userRoutes.js';
+import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
