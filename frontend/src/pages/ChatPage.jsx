7727441ef7d40fbd99ba1854e55433ba159d4d6d
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-08-05 21:21:49 +0530

    Chat section

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router';
+import { getStreamToken } from '../lib/api';
+import { useQuery } from '@tanstack/react-query';
+import useAuthUser from '../hooks/useAuthUser';
+
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import { StreamChat } from "stream-chat";
+import toast from "react-hot-toast";
+
+import ChatLoader from "../components/ChatLoader";
+import CallButton from "../components/CallButton";
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
 
 const ChatPage = () => {
+
+  const {id:targetUserId} = useParams();
+
+  const [chatClient, setChatClient] = useState(null);
+  const [chatChannel, setChatChannel] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const {authUser} = useAuthUser();
+
+  const {data:tokenData} = useQuery({
+    queryKey: ['streamToekn'],
+    queryFn: getStreamToken,
+    enabled: !!authUser,
+  });
+
+  useEffect(() => {
+    const initChat = async () => {
+      if(!tokenData || !authUser) {
+        return;
+      }
+
+      try {
+        console.log("Initializing chat with token:", tokenData.token);
+
+        const client = StreamChat.getInstance(STREAM_API_KEY);
+
+        await client.connectUser({
+          id: authUser._id,
+          name: authUser.fullName,
+          image: authUser.profilePic,
+        }, tokenData.token);
+
+        const channelId = [authUser._id, targetUserId].sort().join("-");
+
+        const currChannel = client.channel("messaging", channelId, {
+          members: [authUser._id, targetUserId],
+        });
+
+        await currChannel.watch();
+        setChatClient(client);
+        setChatChannel(currChannel);
+      } catch (error) {
+        console.error("Error initializing chat:", error);
+        toast.error("Failed to initialize chat. Please try again later.");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    initChat();
+  }, [tokenData, authUser, targetUserId]);
+
+  if(loading || !chatClient || !chatChannel) {
+    return <ChatLoader />;
+  }
+
   return (
-    <div>ChatPage</div>
+    <div className="h-[93vh]">
+      <Chat client={chatClient}>
+        <Channel channel={channel}>
+          <div className="w-full relative">
+            <CallButton handleVideoCall={handleVideoCall} />
+            <Window>
+              <ChannelHeader />
+              <MessageList />
+              <MessageInput focus />
+            </Window>
+          </div>
+          <Thread />
+        </Channel>
+      </Chat>
+    </div>
   )
 }
 
